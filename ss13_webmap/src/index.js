386bbf077cb4e149cbe0bb6c470f98e713b76218
Gesugao-san <gesugao.san@gmail.com>
2021-08-03 17:40:39 +0300

    Learining ReactJS, part 2

--- ss13_webmap/src/index.js
+++ ss13_webmap/src/index.js
@@ -13,18 +13,41 @@ import './index.css';
 */
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        }
+    }
     render() {
         return (
-            <button className="square">
-            {/* TODO */}
+            /*      className="square" onClick={function() { alert('click'); }}> */
+            <button
+                className="square" onClick={
+                () => this.setState({value: 'X'})}
+            >
+                {this.state.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        };
+    }
+
     renderSquare(i) {
-        return <Square />;
+        //return <Square value={i} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
